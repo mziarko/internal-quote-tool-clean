8af34606d3464de0d4b8f206c7f54109a567551a
Mike Ziarko <mikeziarko@mikes-laptop.home>
2025-04-26 09:04:55 -0400

    Force rebuild: tiny change to index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import QuoteTool from '../components/QuoteTool';
+
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Instant Quote</h1>
+      <QuoteTool />
+    </div>
+  );
+}
+
+
+
+
